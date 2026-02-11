ea8657f7a1f6293bbd1a4bd4070c3d6411327157
Maisawa <maisawa@MacBook.local>
2026-02-12 01:10:43 +0900

    fix: redeploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
+import { redirect } from "next/navigation"
+
 export default function Home() {
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>TOPページ表示成功</h1>
-      <p>Next.js は正常に動いています</p>
-    </div>
-  )
+  redirect("/login")
 }
